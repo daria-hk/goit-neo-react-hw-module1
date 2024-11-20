37881d61f3ae7c80746ea6630da7aade18c14a9e
daria_hk <honcharuk.daria95@gmail.com>
2024-11-20 20:28:13 +0100

    add files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,19 @@
+import css from './FriendList.module.css'
+
+const FriendList = ({ friends }) => {
+	return (
+        <ul>
+            {friends.map((friend) => {
+                return <li>
+                    <div>
+                        <img src={friend.avatar} alt="Avatar" width="48" />
+                        <p>{friend.name}</p>
+                        <p>{friend.isOnline}</p>
+                    </div>
+                </li>;
+            })}
+        </ul>
+   );
+}
+
+export default FriendList
